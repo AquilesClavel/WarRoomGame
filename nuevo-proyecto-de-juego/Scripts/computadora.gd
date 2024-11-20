9027c3700f8cb13f1dd21e721d8bfc379b4e81cf
Hector Curiel <man.curiel@hotmail.com>
2024-11-20 14:53:33 -0600

    Ethan y chino completado falta dialogo

--- nuevo-proyecto-de-juego/Scripts/computadora.gd
+++ nuevo-proyecto-de-juego/Scripts/computadora.gd
@@ -82,8 +82,90 @@ func _on_btn_regresar_pressed() -> void:
 	lideres.CamaraCompu.set_enabled(false)
 	lideres.CamaraGeneral.set_enabled(true)
 	
+#FUNCIONES ETHAN
+func _on_ethanus_pressed() -> void:
+	CallChen.show()
+	if primeraUS == false:
+		Dialogic.start("EthanIntroduccion")
+	else:
+		escogerTimelineGringo()
+	Dialogic.timeline_ended.connect(terminoGringo)
 
+func terminoGringo():
+	actualizarEstatusEthanUS()
+	CallEthan.hide()
 
+func actualizarEstatusEthanUS():
+	var estatusUS = btnEthanUS.obtener_estado()
+
+	match estatusUS:
+		"Estado Negativo":
+			print("estado negativo chino")
+			carafelizEthan.hide()
+			caratristeEthan.show()
+			plusEthan.set_self_modulate("ffffff1f")
+			plusplusEthan.set_self_modulate("ffffff1f")
+		"Estado Positivo":
+			print("estado !negativo chino")
+			carafelizEthan.show()
+			caratristeEthan.hide()
+			plusEthan.set_self_modulate("ffffff1f")
+			plusplusEthan.set_self_modulate("ffffff1f")
+		"Estado Muy Positivo":
+			carafelizEthan.show()
+			caratristeEthan.hide()
+			plusEthan.set_self_modulate("ffffff")
+			plusplusEthan.set_self_modulate("ffffff1f")
+		"Estado Extremadamente Positivo":
+			carafelizEthan.show()
+			caratristeEthan.hide()
+			plusEthan.set_self_modulate("ffffff")
+			plusplusEthan.set_self_modulate("ffffff")
+		"Estado Muy Negativo":
+			carafelizEthan.hide()
+			caratristeEthan.show()
+			plusEthan.set_self_modulate("ffffff")
+			plusplusEthan.set_self_modulate("ffffff1f")
+		"Estado Extremadamente Negativo":
+			carafelizEthan.hide()
+			caratristeEthan.show()
+			plusEthan.set_self_modulate("ffffff")
+			plusplusEthan.set_self_modulate("ffffff")
+		"Estado Bloqueado":
+			carafelizEthan.hide()
+			caratristeEthan.hide()
+			caraneutraEthan.show()
+			plusEthan.set_self_modulate("ffffff1f")
+			plusplusEthan.set_self_modulate("ffffff1f")
+			
+	
+	print(estado_actual)
+
+var last_timelineUS = ""
+
+func escogerTimelineGringo():
+	var estatusUS = btnEthanUS.obtener_estado()
+	match estatusUS:
+		"Estado Negativo":
+			last_timelineUS = escoger_random(["EthanNegativo1", "EthanNegativo2"], last_timelineUS)
+		"Estado Positivo":
+			last_timelineUS = escoger_random(["EthanPositivo1", "EthanPositivo2"], last_timelineUS)
+		"Estado Muy Positivo":
+			last_timelineUS = escoger_random(["EthanMuyPositivo1", "EthanMuyPositivo2"], last_timelineUS)
+		"Estado Extremadamente Positivo":
+			last_timelineUS = escoger_random(["EthanExtremadamentePositivo1", "EthanExtremadamentePositivo2"], last_timelineUS)
+		"Estado Muy Negativo":
+			last_timelineUS = escoger_random(["EthanMuyNegativo1", "EthanMuyNegativo2"], last_timelineUS)
+		"Estado Extremadamente Negativo":
+			last_timelineUS = escoger_random(["EthanExtremadamenteNegativo1", "EthanExtremadamenteNegativo2"], last_timelineUS)
+		"Estado Bloqueado":
+			last_timelineUS = escoger_random(["EthanBloqueado1", "EthanBloqueado2"], last_timelineUS)
+
+	print("Timeline seleccionado: ", last_timelineUS)
+	Dialogic.start(last_timelineUS)
+#AQUI TERMINA FUNCIONES ETHAN
+
+#FUNCIONES CHEN 
 func _on_chench_pressed() -> void:
 	CallChen.show()
 	if primeraCH == false:
@@ -144,24 +226,36 @@ func actualizarEstatusChenCH():
 	
 	print(estado_actual)
 
-	
+var last_timelineCH = ""
+
 func escogerTimelineChino():
 	var estatusCH = btnChenCH.obtener_estado()
 	match estatusCH:
 		"Estado Negativo":
-			pass
+			last_timelineCH = escoger_random(["ChenNegativo1", "ChenNegativo2"], last_timelineCH)
 		"Estado Positivo":
-			pass
+			last_timelineCH = escoger_random(["ChenPositivo1", "ChenPositivo2"], last_timelineCH)
 		"Estado Muy Positivo":
-			pass
+			last_timelineCH = escoger_random(["ChenMuyPositivo1", "ChenMuyPositivo2"], last_timelineCH)
 		"Estado Extremadamente Positivo":
-			pass
+			last_timelineCH = escoger_random(["ChenExtremadamentePositivo1", "ChenExtremadamentePositivo2"], last_timelineCH)
 		"Estado Muy Negativo":
-			pass
+			last_timelineCH = escoger_random(["ChenMuyNegativo1", "ChenMuyNegativo2"], last_timelineCH)
 		"Estado Extremadamente Negativo":
-			pass
+			last_timelineCH = escoger_random(["ChenExtremadamenteNegativo1", "ChenExtremadamenteNegativo2"], last_timelineCH)
 		"Estado Bloqueado":
-			pass
+			last_timelineCH = escoger_random(["ChenBloqueado1", "ChenBloqueado2"], last_timelineCH)
+
+	print("Timeline seleccionado: ", last_timelineCH)
+	Dialogic.start(last_timelineCH)
+	
+#AQUI TERMINA FUNCIONES CHEN
+	
+func escoger_random(opciones, ultima_opcion):
+	var nuevo_timeline = ultima_opcion
+	while nuevo_timeline == ultima_opcion:
+		nuevo_timeline = opciones[randi() % opciones.size()]
+	return nuevo_timeline
 
 
 func manejar_victoria(decision:String)->void:
