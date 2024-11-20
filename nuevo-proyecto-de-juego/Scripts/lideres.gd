f2f158b4ba4d0f00df178dc4029584fb01c22a2e
Hector Curiel <man.curiel@hotmail.com>
2024-11-19 22:19:45 -0600

    Computadora

--- nuevo-proyecto-de-juego/Scripts/lideres.gd
+++ nuevo-proyecto-de-juego/Scripts/lideres.gd
@@ -1,13 +1,7 @@
 extends Node2D
 @onready var CamaraGeneral = $CameraGeneral
-@onready var CamaraLider1 = $CamaraLider1
-@onready var CamaraLider2 = $CamaraLider2
-@onready var CamaraLider3 = $CamaraLider3
-@onready var CamaraLider4 = $CamaraLider4
-@onready var btnLider1 = $Control/Lider1
-@onready var btnLider2 = $Control/Lider2
-@onready var btnLider3 = $Control/Lider3
-@onready var btnLider4 = $Control/Lider4
+
+
 
 
 # Called when the node enters the scene tree for the first time.
@@ -24,21 +18,8 @@ func _process(delta: float) -> void:
 	pass
 
 
-func _on_lider_1_pressed() -> void:
-	CamaraGeneral.set_enabled(false)
-	CamaraLider1.set_enabled(true)
 
-
-func _on_lider_2_pressed() -> void:
-	CamaraGeneral.set_enabled(false)
-	CamaraLider2.set_enabled(true)
-
-
-func _on_lider_3_button_down() -> void:
-	CamaraGeneral.set_enabled(false)
-	CamaraLider3.set_enabled(true)
-
-
-func _on_lider_4_button_down() -> void:
-	CamaraGeneral.set_enabled(false)
-	CamaraLider4.set_enabled(true)
+func _on_pcmenu_pressed() -> void:
+	var result;
+	result=get_tree().change_scene_to_file("res://Escenas/Computadora.tscn")
+	print(result)
